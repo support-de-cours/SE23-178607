45a13c7b2daa76e4fd78074e7480b874215cdb14
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-09-26 11:42:13 +0200

    -

---
+++ 05-date/timestamp.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Format Français
+// 12h30m20s 26/09/2023
+
+// Format anglo saxon
+// 2023-26-09 12:30:20 
+
+// Timestamp
+// Represente le temps (en seconde) à partir du 1 janvier 1970 à 00:00:00
+echo time();
