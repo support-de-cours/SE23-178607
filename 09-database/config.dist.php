ac48ce8a97b1b344a7f9f7ff2a2497b499050074
Arnaud BODEL <arnaud.bodel@gmail.com>
2023-09-27 16:42:37 +0200

    -

--- 09-database/config.dist.php
+++ 09-database/config.dist.php
@@ -1,1 +1,8 @@
-
+<?php 
+const DB_DRIVER  = "mysql";         // Type de base de donnée
+const DB_HOST    = "localhost";     // Adresse du serveur de base de données
+const DB_PORT    = 3306;            // Numero de port de base de donnée
+const DB_USER    = "";              // Nom d'utilisateur de la base de donnée
+const DB_PASS    = "";              // Mot de passe de la base de données
+const DB_SCHEMA  = "";              // Nom de la base de donnée
+const DB_CHARSET = "utf8mb4";       // Jeux de caractères par défaut des données
